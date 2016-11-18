d04a3ec22529bdad4970ae8d1d449c207f9a0669
Tyler Collins <tjscollins@gmail.com>
2016-11-18 16:51:53 +1000

    Added localStorage functionality

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,8 +5,8 @@ var TodoList = React.createClass({
   render: function() {
     var {todos} = this.props;
     var renderTodos = () => {
-      return todos.map((todo) => {
-        return (<Todo key={todo.id} {...todo} onToggle={this.props.onToggle}/>);
+      return todos.map((todos) => {
+        return (<Todo key={todos.id} {...todos} onToggle={this.props.onToggle}/>);
       });
     };
     return (
