f489d16c1e857f87feb85c60658d7c78a9cb860a
Tyler Collins <tjscollins@gmail.com>
2016-11-18 10:35:14 +1000

    Complete handleAddTodo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,4 +10,14 @@ describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
+
+  it('should add new todo to the todos state on handleAddTodo', () => {
+    var todoText = 'Shave the cat';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+    expect(todoApp.state.todos.length).toBe(1);
+  });
 });
