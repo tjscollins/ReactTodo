fac1223d52c4c012f887e2b2ff31e48d6cec8a3d
Tyler Collins <tjscollins@gmail.com>
2016-11-21 18:44:14 +1000

    Add actions and action tests

---
+++ app/actions/actions.jsx
@@ -0,0 +1,15 @@
+export var setSearchText = (searchText) => {
+  return {searchText, type: 'SET_SEARCH_TEXT'}
+}
+
+export var addTodo = (text) => {
+  return {type: 'ADD_TODO', text}
+}
+
+export var toggleShowCompleted = () => {
+  return {type: 'TOGGLE_SHOW_COMPLETED'}
+}
+
+export var toggleTodo = (id) => {
+  return {type: 'TOGGLE_TODO', id}
+}
---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,48 @@
+var expect = require('expect');
+var actions = require('actions');
+
+describe('actions', () => {
+  it('should generate search text action', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'Shave the cat'
+    };
+
+    var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate addTodo action', () => {
+    var action = {
+      type: 'ADD_TODO',
+      text: 'Shave the cat'
+    };
+
+    var res = actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggleShowCompleted action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+
+    var res = actions.toggleShowCompleted();
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggleTodo action', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: 3
+    };
+
+    var res = actions.toggleTodo(action.id);
+
+    expect(res).toEqual(action);
+  });
+
+});
